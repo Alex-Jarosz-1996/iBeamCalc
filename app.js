c62297ba0221e5a59b4fdd0befdd18d0b107f67c
alexj <alexj@LAPTOP-7048P6HN>
2023-07-19 17:31:24 +1000

    wired in post method

--- app.js
+++ app.js
@@ -2,13 +2,45 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser'); // Require body-parser
+const ibeam = require('./ibeamCalc'); // Assuming ibeamCalc.js is in the same directory as app.js
 
 // Define the static files folder (assuming your HTML, CSS, and JS files are in the "public" folder)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use body-parser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Define the GET request route for serving the HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Define the POST request route for handling the form submission
+app.post('/submit', (req, res) => {
+  // Access form data using req.body
+  const sigma = req.body.sigma;
+  const forceApplied = req.body.force_applied;
+  const beamThickness = req.body.beam_thick;
+  const numDistances = req.body.num_distances;
+  const distances = Array.from({ length: numDistances }, (_, i) => {
+    const distance = parseFloat(req.body[`distance${i + 1}`]);
+    return isNaN(distance) ? 0 : distance;
+  });
+  
+  console.log('Sigma (N/mm^2):', sigma);
+  console.log('Force Applied (N):', forceApplied);
+  console.log('Beam Thickness (mm):', beamThickness);
+  console.log('Number of Distances:', numDistances);
+  console.log('Distances:', distances);
+
+  // Process the form data as needed (you can add your logic here)
+  const results = ibeam(sigma, forceApplied, beamThickness, distances);
+  console.log(results); // Example: Display the results of the ibeam function
+
+  // Respond with a confirmation message or redirect to another page
+  res.send('Form submission successful!');
 });
 
 // Start the server
---
+++ ibeamCalc.js
@@ -0,0 +1,58 @@
+function ibeam(sigma, forceApplied, beamThick, distances) {
+    const tol = 2.0; // tolerance for the system (how close we want our desired shear modulus to be to the limit)
+    const heightVals = Array.from({ length: 1001 - 2 * beamThick }, (_, i) => i + 1); // Height of I-beam (mm)
+    const widthVals = Array.from({ length: 1001 - 2 * beamThick }, (_, i) => i + 1); // width of I-beam (mm)
+  
+    const returnDataList = distances.map((distance) => {
+      const widthData = [];
+  
+      heightVals.forEach((height) => {
+        const rowData = [];
+  
+        widthVals.forEach((width) => {
+          // calculating moment (Nmm)
+          const momentNm = forceApplied * distance;
+  
+          // calculating shear modulus based on given conditions
+          const zShear = momentNm / sigma;
+  
+          // calculating XSA for each height/w combination
+          const xsa = 2 * beamThick * width + beamThick * height;
+  
+          // calculating ix (i.e. inertia of rotation about the x-axis) for each h/w combination
+          const ix = (beamThick * height ** 3) / 12 + (width / 12) * ((2 * beamThick + height) ** 3 - height ** 3);
+  
+          // calculating neutral axis for each h combination
+          const y = height / 2 + beamThick;
+  
+          // calculating zInt for each Ix/y combination
+          let zInt = ix / y;
+  
+          // check to see if zInt <= zShear or >= tol * zShear since this is not ideal
+          if (zInt <= zShear || zInt >= tol * zShear) {
+            zInt = 0;
+          }
+  
+          // Creating the relationship between zInt and XSA
+          const geom = Math.round(zInt / xsa, 2);
+  
+          rowData.push([height, width, distance, geom]);
+        });
+  
+        widthData.push(...rowData);
+      });
+  
+      // max geom value determines optimal height and width
+      const maxRow = widthData.reduce((prev, curr) => (curr[3] > prev[3] ? curr : prev), widthData[0]);
+  
+      return {
+        height: maxRow[0],
+        width: maxRow[1],
+        distance: maxRow[2],
+      };
+    });
+  
+    return returnDataList;
+  }
+
+module.exports = ibeam; // Export the function to make it accessible to other files
