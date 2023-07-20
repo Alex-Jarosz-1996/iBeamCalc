295f63d41b8c9d4d323ccdac13400c1bb6c403ee
alexj <alexj@LAPTOP-7048P6HN>
2023-07-21 08:48:33 +1000

    cleaned up app.js file

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 // app.js
 const express = require('express');
 const app = express();
-app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('view engine', 'ejs');
 const path = require('path');
-const bodyParser = require('body-parser'); // Require body-parser
-const ibeam = require('./ibeamCalc'); // Assuming ibeamCalc.js is in the same directory as app.js
+const bodyParser = require('body-parser');
+const ibeam = require('./ibeamCalc');
 
-// Define the static files folder (assuming your HTML, CSS, and JS files are in the "public" folder)
+// Define the static files folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Use body-parser middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Define the GET request route for serving the HTML file
+// Define the GET request route for serving the default HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Define the POST request route for handling the form submission
+// Define the POST request route for handling the form submission of
+// ibeam calculation
 app.post('/', (req, res) => {
-  // Access form data using req.body
   const sigma = req.body.sigma;
   const forceApplied = req.body.force_applied;
   const beamThickness = req.body.beam_thick;
@@ -29,18 +29,11 @@ app.post('/', (req, res) => {
     const distance = parseFloat(req.body[`distance${i + 1}`]);
     return isNaN(distance) ? 0 : distance;
   });
-  
-  console.log('Sigma (N/mm^2):', sigma);
-  console.log('Force Applied (N):', forceApplied);
-  console.log('Beam Thickness (mm):', beamThickness);
-  console.log('Number of Distances:', numDistances);
-  console.log('Distances:', distances);
 
-  // Process the form data as needed (you can add your logic here)
+  // ibeam calculation
   const results = ibeam(sigma, forceApplied, beamThickness, distances);
-  console.log(results); // Example: Display the results of the ibeam function
 
-  // Respond with a confirmation message or redirect to another page
+  // redireting to views/distance.ejs file
   res.render('distance', {
     sigma: sigma,
     forceApplied: forceApplied,
@@ -50,7 +43,7 @@ app.post('/', (req, res) => {
 });
 
 // Start the server
-const port = 3000; // Choose the port you prefer
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}/`);
 });
